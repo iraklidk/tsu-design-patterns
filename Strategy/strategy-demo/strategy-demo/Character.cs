00766a6d218e0fc8b8f5c6eb15d5c9180ed4f7d3
Irakli Dolbaia <dolbaia73@gmail.com>
2025-09-28 20:27:34 +0400

    example for strategy pattern

---
+++ Strategy/strategy-demo/strategy-demo/Character.cs
@@ -0,0 +1,19 @@
+﻿namespace strategy_demo
+{
+    internal class Character
+    {
+        private IAttackStrategy _attackStrategy;
+        public Character(IAttackStrategy attackStrategy)
+        {
+            _attackStrategy = attackStrategy;
+        }
+        public void SetAttackStrategy(IAttackStrategy attackStrategy)
+        {
+            _attackStrategy = attackStrategy;
+        }
+        public void PerformAttack()
+        {
+            _attackStrategy.Attack();
+        }
+    }
+}
---
+++ Strategy/strategy-demo/strategy-demo/RangedAttack.cs
@@ -0,0 +1,10 @@
+﻿namespace strategy_demo
+{
+    internal class RangedAttack : IAttackStrategy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Performing a ranged attack!");
+        }
+    }
+}

2eecd323529b57daa33a731d009f4518e025589b
Irakli Dolbaia <dolbaia73@gmail.com>
2025-09-28 16:24:46 +0400

    simple example of the singleton pattern

---
+++ Singleton/singleton-demo/singleton-demo/President.cs
@@ -0,0 +1,28 @@
+﻿namespace singleton_demo
+{
+    public class President
+    {
+        public string name;
+        private static President? _instance;
+        private static readonly object _lock = new object();
+        // Private constructor to prevent instantiation
+        // from outside
+        private President() { }
+        public static President GetInstance(string name)
+        {
+            if (_instance == null)
+            {
+                lock (_lock) // makes sure onlyone thread at a 
+                             // time can enter that code block.
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new President();
+                        _instance.name = name;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Singleton/singleton-demo/singleton-demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace singleton_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance1 = President.GetInstance("anzor");
+            var instance2 = President.GetInstance("kakha"); // will NOT create a new
+                                                            // instance
+            Console.WriteLine("Are instances the same? " + (instance1 == instance2));
+            Console.WriteLine("President's name: " + instance1.name);
+            Console.WriteLine("President's name: " + instance2.name);
+        }
+    }
+}

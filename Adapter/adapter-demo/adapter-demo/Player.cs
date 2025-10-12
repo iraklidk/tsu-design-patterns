6bd5e9e62f04aad2800f1d7366a7fe865707bfee
Irakli Dolbaia <dolbaia73@gmail.com>
2025-10-12 18:43:11 +0400

    adapter

---
+++ Adapter/adapter-demo/adapter-demo/Player.cs
@@ -0,0 +1,25 @@
+﻿// Target interface
+interface IMediaPlayer
+{
+    void Play(string filename);
+}
+
+// Adaptee (incompatible class)
+class VideoPlayer
+{
+    public void PlayVideo(string filename)
+    {
+        Console.WriteLine($"Playing video file: {filename}");
+    }
+}
+
+// Adapter (makes VideoPlayer work with IMediaPlayer)
+class MediaAdapter : IMediaPlayer
+{
+    private VideoPlayer _videoPlayer = new VideoPlayer();
+
+    public void Play(string filename)
+    {
+        _videoPlayer.PlayVideo(filename);
+    }
+}

67def61fe5853960df6d3eedd30627d4a8e32130
iraklidk <dolbaia73@gmail.com>
2025-11-18 00:18:05 +0400

    memento design pattern

---
+++ Memento/memento-demo/memento-demo/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var editor = new TextEditor();
+        var history = new History(editor);
+
+        editor.Text = "Hello";
+        history.Save();
+
+        editor.Text = "Hello World";
+        history.Save();
+
+        editor.Text = "Changed again";
+
+        history.Undo(); // undo to "Hello World"
+        Console.WriteLine(editor.Text);
+    }
+}
---
+++ Memento/memento-demo/memento-demo/TextMemento.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+// ----- Memento -----
+public class TextMemento
+{
+    public string SavedText { get; }
+
+    public TextMemento(string text) => SavedText = text;
+}
+
+// ----- Originator -----
+public class TextEditor
+{
+    public string Text { get; set; }
+
+    public TextMemento Save() => new TextMemento(Text);
+
+    public void Restore(TextMemento memento) => Text = memento.SavedText;
+}
+
+// ----- Caretaker with Undo -----
+public class History
+{
+    private Stack<TextMemento> _stack = new Stack<TextMemento>();
+    private TextEditor _editor;
+
+    public History(TextEditor editor) => _editor = editor;
+
+    public void Save() => _stack.Push(_editor.Save());
+    public void Undo()
+    {
+        if (_stack.Count == 0)
+            return;
+
+        var memento = _stack.Pop();
+        _editor.Restore(memento);
+    }
+}

379a656309312ffa4263bc32198217f66652f8ef
iraklidk <dolbaia73@gmail.com>
2025-12-16 03:08:05 +0400

    interpreter

---
+++ Interpreter/interpreter-demo/interpreter-demo/AddExpression.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+public class AddExpression : IExpression
+{
+    private readonly IExpression _left;
+    private readonly IExpression _right;
+
+    public AddExpression(IExpression left, IExpression right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public int Interpret()
+    {
+        return _left.Interpret() + _right.Interpret();
+    }
+}
+
+public interface IExpression
+{
+    int Interpret();
+}
---
+++ Interpreter/interpreter-demo/interpreter-demo/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Represents: 5 + 3
+        IExpression five = new NumberExpression(5);
+        IExpression three = new NumberExpression(3);
+
+        IExpression add = new AddExpression(five, three);
+
+        Console.WriteLine(add.Interpret()); // 8
+    }
+}
